538f8af5971a08a6bb10eb161748de6ba031804c
tengahcang <cangg522@gmail.com>
2023-05-17 14:11:32 +0700

    ini baru

---
+++ database/seeders/PositionSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class PositionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('positions')->insert([
+            [
+                'code' => 'FE',
+            ],
+        ]);
+    }
+}
