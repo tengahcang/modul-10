6538c53ed0e88fd2806ee9a98df550a4dfc358af
tengahcang <cangg522@gmail.com>
2023-06-25 20:29:24 +0700

    ini sudah selesai

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('users')->insert([
+            'name' => 'admin',
+            'email' => 'cangg522@gmail.com',
+            'password' => bcrypt('asu12345')
+        ]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\EmployeeController;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,6 +20,12 @@ use App\Http\Controllers\EmployeeController;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('home', [HomeController::class, 'index'])->name('home');
-Route::get('profile', ProfileController::class)->name('profile');
-Route::resource('employees', EmployeeController::class);
+
+
+Auth::routes();
+
+Route::group(['middleware'=>'auth'],function(){
+    Route::get('home', [HomeController::class, 'index'])->name('home');
+    Route::get('profile', ProfileController::class)->name('profile');
+    Route::resource('employees', EmployeeController::class);
+});
